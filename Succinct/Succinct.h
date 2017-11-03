7a1c32b3bf5317428300b4a14c7b0c80f9b4c9ab
Derek <dereklee@downtothis.com>
2017-11-04 07:52:14 +0900

    Initial Commit

---
+++ Succinct/Succinct.h
@@ -0,0 +1,19 @@
+//
+//  Succinct.h
+//  Succinct
+//
+//  Created by Derek Lee on 11/4/17.
+//  Copyright © 2017 Sun Rising App Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Succinct.
+FOUNDATION_EXPORT double SuccinctVersionNumber;
+
+//! Project version string for Succinct.
+FOUNDATION_EXPORT const unsigned char SuccinctVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Succinct/PublicHeader.h>
+
+
