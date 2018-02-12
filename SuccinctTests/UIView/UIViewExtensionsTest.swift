4db8287c0c3697305df4b22b274be5e83072def3
Derek <dereklee@downtothis.com>
2018-02-13 08:00:24 +0900

    Converts XCTest over to Quick test

--- SuccinctTests/UIView/UIViewExtensionsTest.swift
+++ SuccinctTests/UIView/UIViewExtensionsTest.swift
@@ -1,37 +1,43 @@
 import XCTest
+import Quick
+import Nimble
 @testable import Succinct
 
-class UIViewExtensionsTest: XCTestCase {
-    func test_findButton_whenButtonExistsAsFirstSubview() {
-        let viewController = UIViewControllerBuilder()
-            .withSubview(
-                UIButtonBuilder().withTitleText("Login").build())
-            .build()
-        
-        
-        let result = viewController.view.findButton(withText: "Login")
-        
-        
-        XCTAssertNotNil(result)
-        XCTAssertEqual(result?.titleLabel?.text, "Login")
-    }
-    
-    func test_findButton_whenButtonExistsAsSecondSubview() {
-        let viewController = UIViewControllerBuilder()
-            .withSubview(
-                UIViewBuilder()
+class UIViewExtensionsSpec: QuickSpec {
+    override func spec() {
+        describe("finding buttons") {
+            it("can find a button when it exists in the first subview") {
+                let viewController = UIViewControllerBuilder()
                     .withSubview(
-                        UIButtonBuilder().withTitleText("Login").build()
+                        UIButtonBuilder().withTitleText("Login").build())
+                    .build()
+
+
+                let result = viewController.view.findButton(withText: "Login")
+
+
+                XCTAssertNotNil(result)
+                XCTAssertEqual(result?.titleLabel?.text, "Login")
+            }
+
+            it("can find a button when it exists in the second subview") {
+                let viewController = UIViewControllerBuilder()
+                    .withSubview(
+                        UIViewBuilder()
+                            .withSubview(
+                                UIButtonBuilder().withTitleText("Login").build()
+                            )
+                            .build()
                     )
                     .build()
-            )
-            .build()
-        
-        
-        let result = viewController.view.findButton(withText: "Login")
-        
-        
-        XCTAssertNotNil(result)
-        XCTAssertEqual(result?.titleLabel?.text, "Login")
+
+
+                let result = viewController.view.findButton(withText: "Login")
+
+
+                XCTAssertNotNil(result)
+                XCTAssertEqual(result?.titleLabel?.text, "Login")
+            }
+        }
     }
 }
