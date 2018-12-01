39d604c29c2de4a82e5b15c6a2d185b1deac95d9
Derek Lee <dereklee@downtothis.com>
2018-12-01 16:33:19 +0900

    Backfill some tests for tapping UIButton for the negative case to ensure we have coverage in case anything changes here

--- SuccinctContainerTests/UIViewController/UIViewController+UIButtonSpec.swift
+++ SuccinctContainerTests/UIViewController/UIViewController+UIButtonSpec.swift
@@ -10,6 +10,41 @@ final class UIViewController_UIButtonSpec: QuickSpec {
 
     override func spec() {
         describe("button functionality") {
+            beforeEach {
+                self.buttonWasTapped = false
+            }
+            
+            it("cannot tap a button when no buttons exist") {
+                let viewController = UIViewControllerBuilder().build()
+                viewController.loadViewControllerForUnitTest()
+                
+                
+                viewController.tapButton(withExactText: "Login")
+                
+                
+                expect(self.buttonWasTapped).to(beFalse())
+            }
+
+            it("cannot tap a button when the button text does not match") {
+                let targetAction = TargetAction(self.unitTestButtonWasTapped)
+                
+                let loginButton = UIButtonBuilder()
+                    .withTitleText("Login")
+                    .withTargetAction(targetAction)
+                    .build()
+                
+                let viewController = UIViewControllerBuilder()
+                    .withSubview(loginButton)
+                    .build()
+                viewController.loadViewControllerForUnitTest()
+                
+                
+                viewController.tapButton(withExactText: "ABC")
+                
+                
+                expect(self.buttonWasTapped).to(beFalse())
+            }
+            
             it("can tap a button with exact text") {
                 let targetAction = TargetAction(self.unitTestButtonWasTapped)
 
