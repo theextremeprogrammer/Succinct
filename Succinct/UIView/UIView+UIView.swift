f6bce93d331202d32ddd56ffb3bf29eaf51ab1a3
Derek Lee <dereklee@downtothis.com>
2018-12-05 08:36:44 +0900

    Moves countOfImages: to UIView+UIImage

--- Succinct/UIView/UIView+UIView.swift
+++ Succinct/UIView/UIView+UIView.swift
@@ -62,31 +62,4 @@ extension UIView {
 
         return viewsCounted
     }
-
-    public func countOfImages(_ searchImage: UIImage) -> Int {
-        var imagesCounted = 0
-
-        for subview in subviews {
-            if let imageView = subview as? UIImageView,
-                imageView.image == searchImage {
-                imagesCounted += 1
-            } else if let collectionView = subview as? UICollectionView {
-                let numberOfSections = collectionView.numberOfSections
-                for section in 0..<numberOfSections {
-                    let numberOfItems = collectionView.numberOfItems(inSection: section)
-                    for item in 0..<numberOfItems {
-                        let indexPath = IndexPath(item: item, section: section)
-                        if let cell = collectionView.dataSource?
-                            .collectionView(collectionView, cellForItemAt: indexPath) {
-                            imagesCounted += cell.countOfImages(searchImage)
-                        }
-                    }
-                }
-            }
-
-            imagesCounted += subview.countOfImages(searchImage)
-        }
-
-        return imagesCounted
-    }
 }
