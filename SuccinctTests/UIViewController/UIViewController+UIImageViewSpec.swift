40a15e9c784064d02dc06ae4cc9b207d70bbbb5d
Derek Lee <dereklee@downtothis.com>
2018-12-05 11:15:34 +0900

    Improves tests for containsImage: and getImageView:
    - Tests for the case when there is an image that is not the image that is being searched for

--- SuccinctTests/UIViewController/UIViewController+UIImageViewSpec.swift
+++ SuccinctTests/UIViewController/UIViewController+UIImageViewSpec.swift
@@ -5,11 +5,12 @@ import Succinct
 final class UIViewController_UIImageViewSpec: QuickSpec {
     override func spec() {
         let searchImage = UIImage(assetIdentifier: .obligatoryCatImage)!
+        let otherImage = UIImage(assetIdentifier: .obligatoryFoliageImage)!
 
         describe("finding a view that contains an image") {
             it("knows when the search image cannot be found in the view hierarchy") {
                 let viewController = UIViewControllerBuilder()
-                    .withSubview(UIViewBuilder().build())
+                    .withSubview(UIViewBuilder().withImageView(otherImage).build())
                     .build()
 
 
@@ -24,26 +25,42 @@ final class UIViewController_UIImageViewSpec: QuickSpec {
                             .build()
                     )
                     .build()
-
-
+                
+                
                 expect(viewController.containsImage(searchImage)).to(beTrue())
             }
         }
-
+        
         describe("retrieving an imageview for the specified image") {
-            it("can retrieve the imageview containing the specified image found within the view hierarchy") {
-                let viewController = UIViewControllerBuilder()
-                    .withSubview(
-                        UIViewBuilder()
-                            .withImageView(searchImage)
-                            .build()
-                    )
-                    .build()
+            context("when the imageview containing the specified image CANNOT be found within the view hierarchy") {
+                it("does not return an image") {
+                    let viewController = UIViewControllerBuilder()
+                        .withSubview(UIViewBuilder().withImageView(otherImage).build())
+                        .build()
+                    
+                    
+                    let maybeImageView = viewController.getImageView(for: searchImage)
+                    expect(maybeImageView).to(beNil())
+                }
+            }
+            
+            context("when the imageview containing the specified image can be found within the view hierarchy") {
+                it("returns the image") {
+                    let viewController = UIViewControllerBuilder()
+                        .withSubview(
+                            UIViewBuilder()
+                                .withImageView(searchImage)
+                                .build()
+                        )
+                        .build()
+                    
+                    
+                    let maybeImageView = viewController.getImageView(for: searchImage)
+                    expect(maybeImageView).to(beAKindOf(UIImageView.self))
+                    expect(maybeImageView?.isHidden).to(beFalse())
+                }
                 
                 
-                let maybeImageView = viewController.getImageView(for: searchImage)
-                expect(maybeImageView).to(beAKindOf(UIImageView.self))
-                expect(maybeImageView?.isHidden).to(beFalse())
             }
         }
     }
