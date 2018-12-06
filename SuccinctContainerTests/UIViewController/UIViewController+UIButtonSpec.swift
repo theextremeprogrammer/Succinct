c5125d42a573586940c1328cfef4a666b58f7a9f
Canyon Boak <immenor@gmail.com>
2018-12-06 15:25:26 +0900

    Adds tests in container for tapping buttons inside of table view cells

--- SuccinctContainerTests/UIViewController/UIViewController+UIButtonSpec.swift
+++ SuccinctContainerTests/UIViewController/UIViewController+UIButtonSpec.swift
@@ -65,6 +65,37 @@ final class UIViewController_UIButtonSpec: QuickSpec {
                     
                     expect(self.buttonWasTapped).to(beTrue())
                 }
+
+                it("can tap a button with exact text inside of a table view cell") {
+                    let targetAction = TargetAction(self.unitTestButtonWasTapped)
+
+                    let loginButton = UIButtonBuilder()
+                        .withTitleText("Login")
+                        .withTargetAction(targetAction)
+                        .build()
+
+                    let viewController = UIViewControllerBuilder()
+                        .withSubview(
+                            UITableViewBuilder()
+                                .withCellConfiguration(
+                                    UITableViewCellConfiguration(
+                                        indexPath: IndexPath(row: 0, section: 0),
+                                        titleLabelText: "Action",
+                                        selected: false,
+                                        subviews: [loginButton]
+                                    )
+                                )
+                                .build()
+                        )
+                        .build()
+                    viewController.loadViewControllerForUnitTest()
+
+
+                    viewController.tapButton(withExactText: "Login")
+
+
+                    expect(self.buttonWasTapped).to(beTrue())
+                }
             }
             
             context("tapping a button with an image") {
@@ -109,6 +140,39 @@ final class UIViewController_UIButtonSpec: QuickSpec {
                     viewController.tapButton(withImage: image)
                     
                     
+                    expect(self.buttonWasTapped).to(beTrue())
+                }
+
+                it("can tap a button with an image inside of a table view cell") {
+                    let targetAction = TargetAction(self.unitTestButtonWasTapped)
+
+                    let image = UIImage(assetIdentifier: .obligatoryCatImage)!
+
+                    let loginButton = UIButtonBuilder()
+                        .withImage(image)
+                        .withTargetAction(targetAction)
+                        .build()
+
+                    let viewController = UIViewControllerBuilder()
+                        .withSubview(
+                            UITableViewBuilder()
+                                .withCellConfiguration(
+                                    UITableViewCellConfiguration(
+                                        indexPath: IndexPath(row: 0, section: 0),
+                                        titleLabelText: "Action",
+                                        selected: false,
+                                        subviews: [loginButton]
+                                    )
+                                )
+                                .build()
+                        )
+                        .build()
+                    viewController.loadViewControllerForUnitTest()
+
+
+                    viewController.tapButton(withImage: image)
+
+
                     expect(self.buttonWasTapped).to(beTrue())
                 }
             }
