039c506bd78eabef28d065de6c6a8e58fb981019
Derek Lee <dereklee@downtothis.com>
2018-12-11 13:54:48 +0900

    Refactors findTextField(...) to utilize findInSubviews

--- Succinct/UIView/UIView+UITextField.swift
+++ Succinct/UIView/UIView+UITextField.swift
@@ -22,46 +22,68 @@ extension UIView {
 // MARK: - Finding UITextFields
 extension UIView {
     public func findTextField(withExactPlaceholderText searchText: String) -> UITextField? {
-        for subview in subviews {
-            if let textField = subview as? UITextField,
-                textField.placeholder == searchText {
-                return textField
-            }
-
-            if let textField = subview.findTextField(withExactPlaceholderText: searchText) {
-                return textField
-            }
-        }
-
-        return nil
+        return findInSubviews(satisfyingCondition: { $0.isTextField(withExactPlaceholderText: searchText) }) as? UITextField
     }
 
     public func findTextField(containingPlaceholderText searchText: String) -> UITextField? {
-        return subviews
-            .compactMap { $0 as? UITextField ?? $0.findTextField(containingPlaceholderText: searchText) }
-            .filter { $0.placeholder?.contains(searchText) ?? false }
-            .first
+        return findInSubviews(satisfyingCondition: { $0.isTextField(containingPlaceholderText: searchText) }) as? UITextField
     }
 
     public func findTextField(withExactText searchText: String) -> UITextField? {
-        for subview in subviews {
-            if let textField = subview as? UITextField,
-                textField.text == searchText {
-                return textField
-            }
-
-            if let textField = subview.findTextField(withExactText: searchText) {
-                return textField
-            }
-        }
-
-        return nil
+        return findInSubviews(satisfyingCondition: { $0.isTextField(withExactText: searchText) }) as? UITextField
     }
 
     public func findTextField(containingText searchText: String) -> UITextField? {
-        return subviews
-            .compactMap { $0 as? UITextField ?? $0.findTextField(containingText: searchText) }
-            .filter { $0.text?.contains(searchText) ?? false }
-            .first
+        return findInSubviews(satisfyingCondition: { $0.isTextField(containingText: searchText) }) as? UITextField
+    }
+}
+
+fileprivate extension UIView {
+    func isTextField(withExactPlaceholderText searchText: String) -> Bool {
+        guard let textfield = self as? UITextField else {
+            return false
+        }
+        
+        guard let placeholderText = textfield.placeholder else {
+            return false
+        }
+        
+        return placeholderText == searchText
+    }
+    
+    func isTextField(containingPlaceholderText searchText: String) -> Bool {
+        guard let textfield = self as? UITextField else {
+            return false
+        }
+        
+        guard let placeholderText = textfield.placeholder else {
+            return false
+        }
+        
+        return placeholderText.contains(searchText)
+    }
+
+    func isTextField(withExactText searchText: String) -> Bool {
+        guard let textfield = self as? UITextField else {
+            return false
+        }
+        
+        guard let text = textfield.text else {
+            return false
+        }
+        
+        return text == searchText
+    }
+
+    func isTextField(containingText searchText: String) -> Bool {
+        guard let textfield = self as? UITextField else {
+            return false
+        }
+        
+        guard let text = textfield.text else {
+            return false
+        }
+
+        return text.contains(searchText)
     }
 }
