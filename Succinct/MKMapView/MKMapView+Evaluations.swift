b917095e931d79da9b07da51add8a0691de69e87
Derek Lee <dereklee@downtothis.com>
2019-02-16 15:34:33 +0900

    Adds support for findMapView() from UIViewController
    - Closes #11

--- Succinct/MKMapView/MKMapView+Evaluations.swift
+++ Succinct/MKMapView/MKMapView+Evaluations.swift
@@ -1,5 +1,11 @@
+import MapKit
+
 extension UIViewController {
     public func hasMapView() -> Bool {
         return view.findMapView().isNotNil()
+    }
+
+    public func findMapView() -> MKMapView? {
+        return view.findMapView()
     }
 }
--- SuccinctTests/UIViewController/UIViewController+MKMapViewSpec.swift
+++ SuccinctTests/UIViewController/UIViewController+MKMapViewSpec.swift
@@ -7,7 +7,7 @@ final class UIViewController_MKMapViewSpec: QuickSpec {
     override func spec() {
         var viewController: UIViewController!
 
-        describe("finding map views by map type") {
+        describe("determining if a map view exists") {
             context("when an MKMapView does not exist") {
                 it("does not find an MKMapView") {
                     viewController = UIViewControllerBuilder().build()
@@ -48,6 +48,51 @@ final class UIViewController_MKMapViewSpec: QuickSpec {
 
 
                     expect(viewController.hasMapView()).to(beTrue())
+                }
+            }
+        }
+
+        describe("retrieving a map view if it exists") {
+            context("when an MKMapView does not exist") {
+                it("does not return a MKMapView") {
+                    viewController = UIViewControllerBuilder().build()
+
+
+                    expect(viewController.findMapView()).to(beNil())
+                }
+            }
+
+            context("when an MKMapView exists in the first subview hierarchy") {
+                it("returns the MKMapView") {
+                    let mapView = MKMapViewBuilder().build()
+
+                    viewController = UIViewControllerBuilder()
+                        .withSubview(mapView)
+                        .build()
+
+
+                    expect(viewController.findMapView()).toNot(beNil())
+                }
+            }
+
+            context("when an MKMapView exists in the second subview hierarchy") {
+                it("returns the MKMapView") {
+                    let mapView = MKMapViewBuilder().build()
+
+                    viewController = UIViewControllerBuilder()
+                        .withSubview(UIView())
+                        .withSubview(
+                            UIViewBuilder()
+                                .withSubview(UIView())
+                                .withSubview(mapView)
+                                .withSubview(UIView())
+                                .build()
+                        )
+                        .withSubview(UIView())
+                        .build()
+
+
+                    expect(viewController.findMapView()).toNot(beNil())
                 }
             }
         }
