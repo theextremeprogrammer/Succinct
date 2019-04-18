04a9623ac6e758956098bf2a0c82b96d8b367486
Derek Lee <dereklee@downtothis.com>
2019-04-19 08:44:42 +0900

    Re-orders logic in searching the navigation controller

--- Succinct/UINavigationController/UINavigationController+UILabel.swift
+++ Succinct/UINavigationController/UINavigationController+UILabel.swift
@@ -2,11 +2,11 @@ import UIKit
 
 extension UINavigationController {
     func hasLabelInNavigationBar(withExactText searchText: String) -> Bool? {
-        if let titleView = navigationItem.titleView, titleView.hasLabel(withExactText: searchText) {
+        if view.hasLabel(withExactText: searchText) {
             return true
         }
 
-        if view.hasLabel(withExactText: searchText) {
+        if let titleView = navigationItem.titleView, titleView.hasLabel(withExactText: searchText) {
             return true
         }
 
@@ -14,11 +14,11 @@ extension UINavigationController {
     }
 
     func hasLabelInNavigationBar(containingText searchText: String) -> Bool? {
-        if let titleView = navigationItem.titleView, titleView.hasLabel(containingText: searchText) {
+        if view.hasLabel(containingText: searchText) {
             return true
         }
 
-        if view.hasLabel(containingText: searchText) {
+        if let titleView = navigationItem.titleView, titleView.hasLabel(containingText: searchText) {
             return true
         }
 
@@ -26,30 +26,28 @@ extension UINavigationController {
     }
 
     func findLabelInNavigationBar(withExactText searchText: String) -> UILabel? {
-        if  let titleView = navigationItem.titleView,
+        if let labelInNavCtrlHierarchy = view.findLabel(withExactText: searchText) {
+            return labelInNavCtrlHierarchy
+        }
+
+        if let titleView = navigationItem.titleView,
             let labelInTitleView = titleView.findLabel(withExactText: searchText) {
 
             return labelInTitleView
-        }
-
-
-        if let labelInNavCtrlHierarchy = view.findLabel(withExactText: searchText) {
-            return labelInNavCtrlHierarchy
         }
 
         return nil
     }
 
     func findLabelInNavigationBar(containingText searchText: String) -> UILabel? {
-        if  let titleView = navigationItem.titleView,
+        if let labelInNavCtrlHierarchy = view.findLabel(containingText: searchText) {
+            return labelInNavCtrlHierarchy
+        }
+
+        if let titleView = navigationItem.titleView,
             let labelInTitleView = titleView.findLabel(containingText: searchText) {
 
             return labelInTitleView
-        }
-
-
-        if let labelInNavCtrlHierarchy = view.findLabel(containingText: searchText) {
-            return labelInNavCtrlHierarchy
         }
 
         return nil
