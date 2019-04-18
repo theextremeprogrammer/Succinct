41deb9bb39599d42938a51aa8a3d40f3ab11845f
Derek Lee <dereklee@downtothis.com>
2019-04-19 08:46:52 +0900

    Adds documentation to UIViewController+UILabel methods

--- Succinct/UIViewController/UIViewController+UILabel.swift
+++ Succinct/UIViewController/UIViewController+UILabel.swift
@@ -2,6 +2,20 @@ import UIKit
 
 // MARK: - Has UILabel
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UILabel whose text matches the searchText exactly.
+    ///
+    /// In addition to the view hierarchy, the following locations are also searched:
+    /// - The view controller's title property
+    /// - The navigation item's title view hierarchy
+    /// - The navigation controller's view hierarchy
+    /// - The navigation controller's navigation item's title view hierarchy
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: A boolean value indicating if a label was found or not.
+    ///
     public func hasLabel(withExactText searchText: String) -> Bool {
         if view.hasLabel(withExactText: searchText) {
             return true
@@ -18,6 +32,20 @@ extension UIViewController {
         return navigationController?.hasLabelInNavigationBar(withExactText: searchText) ?? false
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UILabel whose text contains the searchText.
+    ///
+    /// In addition to the view hierarchy, the following locations are also searched:
+    /// - The view controller's title property
+    /// - The navigation item's title view hierarchy
+    /// - The navigation controller's view hierarchy
+    /// - The navigation controller's navigation item's title view hierarchy
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: A boolean value indicating if a label was found or not.
+    ///
     public func hasLabel(containingText searchText: String) -> Bool {
         if view.hasLabel(containingText: searchText) {
             return true
@@ -35,8 +63,21 @@ extension UIViewController {
     }
 }
 
-// MARK: - Finding UILabel
+// MARK: - Find UILabel
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UILabel whose text matches the searchText exactly.
+    ///
+    /// In addition to the view hierarchy, the following locations are also searched:
+    /// - The navigation item's title view hierarchy
+    /// - The navigation controller's view hierarchy
+    /// - The navigation controller's navigation item's title view hierarchy
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: An optional UILabel, if one is found.
+    ///
     public func findLabel(withExactText searchText: String) -> UILabel? {
         let labelInRootView = view.findLabel(withExactText: searchText)
         if labelInRootView.isNotNil() {
@@ -50,6 +91,19 @@ extension UIViewController {
         return navigationController?.findLabelInNavigationBar(withExactText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UILabel whose text contains the searchText.
+    ///
+    /// In addition to the view hierarchy, the following locations are also searched:
+    /// - The navigation item's title view hierarchy
+    /// - The navigation controller's view hierarchy
+    /// - The navigation controller's navigation item's title view hierarchy
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: An optional UILabel, if one is found.
+    ///
     public func findLabel(containingText searchText: String) -> UILabel? {
         let labelInRootView = view.findLabel(containingText: searchText)
         if labelInRootView.isNotNil() {
