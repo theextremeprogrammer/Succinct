d7a347414532cc5753a2fadd0cef0cc1901272e5
Derek Lee <dereklee@downtothis.com>
2019-04-19 08:18:41 +0900

    Updates documentation for findButton(withExactText:)

--- Succinct/UIViewController/UIViewController+UIButton.swift
+++ Succinct/UIViewController/UIViewController+UIButton.swift
@@ -2,33 +2,39 @@ import UIKit
 
 // MARK: - Searching by exact text
 extension UIViewController {
-    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title matches the searchText exactly.
     ///
-    /// - Note: Once a UIButton is found with the provided searchText the remainder of the view hierarchy **is not searched**.
+    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title for `UIControl.State.normal` matches the searchText exactly.
     ///
-    /// - Parameter searchText: Text to compare to the title of any UIButtons that are found.
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the title for `UIControl.State.normal` of any UIButtons that are found.
     /// - Returns: A boolean value indicating if a button was found or not.
+    ///
     public func hasButton(withExactText searchText: String) -> Bool {
         return view
             .findButton(withExactText: searchText)
             .isNotNil()
     }
 
-    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title matches the searchText exactly.
     ///
-    /// - Note: Once a UIButton is found with the provided searchText the remainder of the view hierarchy **is not searched**.
+    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title for `UIControl.State.normal` matches the searchText exactly.
     ///
-    /// - Parameter searchText: Text to compare to the title of any UIButtons that are found.
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the title for `UIControl.State.normal` of any UIButtons that are found.
     /// - Returns: An optional UIButton, if one is found.
+    ///
     public func findButton(withExactText searchText: String) -> UIButton? {
         return view.findButton(withExactText: searchText)
     }
 
-    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title matches the searchText exactly and then attempts to tap the button by sending a `.touchUpInside` event to it.
     ///
-    /// - Note: Once a UIButton is found with the provided searchText the remainder of the view hierarchy **is not searched**.
+    /// Searches the entire view hierarchy of a view controller's primary view for a UIButton whose title for `UIControl.State.normal` matches the searchText exactly and then attempts to tap the button by sending a `.touchUpInside` event to it.
     ///
-    /// - Parameter searchText: Text to compare to the title of any UIButtons that are found.
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the title for `UIControl.State.normal` of any UIButtons that are found.
+    ///
     public func tapButton(withExactText searchText: String) {
         view.findButton(withExactText: searchText)?
             .tapAndFireTargetEvent()
