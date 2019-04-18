7251e7ef23275f15cdf7f11f8d2c5134f26e19d2
Derek Lee <dereklee@downtothis.com>
2019-04-19 08:31:58 +0900

    Adds documentation to UIView+UIButton methods

--- Succinct/UIView/UIButton/UIView+UIButton.swift
+++ Succinct/UIView/UIButton/UIView+UIButton.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UIButton whose title for `UIControl.State.normal` matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the title for `UIControl.State.normal` of any UIButton objects that are found.
+    /// - Returns: An optional UIButton, if one is found.
+    ///
     public func findButton(withExactText searchText: String) -> UIButton? {
         return findInSubviews(
             satisfyingCondition: SuccinctCondition {
@@ -9,6 +17,14 @@ extension UIView {
         ) as? UIButton
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UIButton whose image for `UIControl.State.normal` matches the searchImage.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchImage: Image to compare to the image for `UIControl.State.normal` of any UIButton objects that are found.
+    /// - Returns: An optional UIButton, if one is found.
+    ///
     public func findButton(withImage searchImage: UIImage) -> UIButton? {
         return findInSubviews(
             satisfyingCondition: SuccinctCondition {
@@ -17,6 +33,12 @@ extension UIView {
         ) as? UIButton
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for all UIButton objects whose state matches the indicated state.
+    ///
+    /// - Parameter state: The `UIControl.State` to compare to any UIButton objects that are found.
+    /// - Returns: An array of UIButton objects.
+    ///
     public func findButtons(withState searchState: UIControl.State) -> [UIButton] {
         var buttons: [UIButton] = []
 
