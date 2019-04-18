19d1ba120df2094e3d17877b4b97d06df71fa3a3
Derek Lee <dereklee@downtothis.com>
2019-04-18 21:25:27 +0900

    Adjusts jazzy settings for documentation

--- bin/generate_jazzy_docs.sh
+++ bin/generate_jazzy_docs.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 jazzy \
   --clean \
+  --min-acl public \
+  --no-hide-documentation-coverage \
   --author "Derek Lee" \
   --author_url https://github.com/derekleerock \
   --github_url https://github.com/derekleerock/Succinct \
   --xcodebuild-arguments -scheme,Succinct \
   --module Succinct \
-  --output docs
+  --output ./docs \
+  --documentation=./*.md
