c6c6a0a6d3f268fba55a30ba3ba943c6e51ecdea
Derek Lee <dereklee@downtothis.com>
2019-04-20 15:16:42 +0900

    Adds documentation to methods in the UIView+UILabel file

--- Succinct/UIView/UILabel/UIView+UILabel.swift
+++ Succinct/UIView/UILabel/UIView+UILabel.swift
@@ -2,10 +2,26 @@ import UIKit
 
 // MARK: - Has UILabels
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UILabel whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: A boolean value indicating if a label was found or not.
+    ///
     public func hasLabel(withExactText searchText: String) -> Bool {
         return findLabel(withExactText: searchText).isNotNil()
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UILabel whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: A boolean value indicating if a label was found or not.
+    ///
     public func hasLabel(containingText searchText: String) -> Bool {
         return findLabel(containingText: searchText).isNotNil()
     }
@@ -13,12 +29,28 @@ extension UIView {
 
 // MARK: - Finding UILabels
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UILabel whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: An optional UILabel, if one is found.
+    ///
     public func findLabel(withExactText searchText: String) -> UILabel? {
         return findInSubviews(
             satisfyingCondition: SuccinctCondition { $0.isLabel(withExactText: searchText) }
         ) as? UILabel
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UILabel whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UILabel objects that are found.
+    /// - Returns: An optional UILabel, if one is found.
+    ///
     public func findLabel(containingText searchText: String) -> UILabel? {
         return findInSubviews(
             satisfyingCondition: { $0.isLabel(containingText: searchText) }
