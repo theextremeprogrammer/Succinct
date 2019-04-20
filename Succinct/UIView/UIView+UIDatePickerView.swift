3411da3826fda9d20b04429fef49ce3a1518d31a
Derek Lee <dereklee@downtothis.com>
2019-04-20 15:29:12 +0900

    Adds documentation to all UIDatePicker related methods.

--- Succinct/UIView/UIView+UIDatePickerView.swift
+++ Succinct/UIView/UIView+UIDatePickerView.swift
@@ -1,6 +1,13 @@
 import UIKit
 
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UIDatePicker object.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Returns: An optional UIDatePicker, if one is found.
+    ///
     public func findDatePickerView() -> UIDatePicker? {
         return findInSubviews(
             satisfyingCondition: { $0.isDatePickerView() }
@@ -9,6 +16,13 @@ extension UIView {
 }
 
 extension UIView {
+    ///
+    /// When searching through the view hierarchy of a view, there appears to be times when some of Apple's UIKit widgets contain an innfinite number of subviews. When these are encountered, Succinct can get stuck within this infinite view hierarchy causing problems when running tests.
+    ///
+    /// Currently the only UIKit object that has been discovered to have an infinite number of views is a UIDatePicker object. If any others are found, they should be included here.
+    ///
+    /// - Returns: A boolean value indicating if the current UIView object is a type of UIKit object that contains an infinite number of subviews.
+    ///
     public var isNotATypeThatContainsAnInfiniteNumberOfSubviews: Bool {
         get {
             return isDatePickerView() == false
--- Succinct/UIViewController/UIViewController+UIDatePickerView.swift
+++ Succinct/UIViewController/UIViewController+UIDatePickerView.swift
@@ -1,10 +1,24 @@
 import UIKit
 
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UIDatePicker object.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Returns: A boolean value indicating if an UIDatePicker was found or not.
+    ///
     public func hasDatePickerView() -> Bool {
         return view.findDatePickerView().isNotNil()
     }
     
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UIDatePicker object.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Returns: An optional UIDatePicker, if one is found.
+    ///
     public func findDatePickerView() -> UIDatePicker? {
         return view.findDatePickerView()
     }
