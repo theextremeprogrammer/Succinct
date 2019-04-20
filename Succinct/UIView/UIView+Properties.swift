be291770810c034c0fa6f35f87515e2c566f707e
Derek Lee <dereklee@downtothis.com>
2019-04-20 17:51:43 +0900

    Adds documentation for UIView.isVisible property

--- Succinct/UIView/UIView+Properties.swift
+++ Succinct/UIView/UIView+Properties.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 extension UIView {
+    ///
+    /// Convenience method which indicates if the view is visible or not. This can be helpful when writing assertions as opposed to testing the isHidden property.
+    ///
     public var isVisible: Bool {
         get {
             return !isHidden
