c85d9af50600b0dd6e2df02da4c875f3986cc849
Derek Lee <dereklee@downtothis.com>
2019-04-20 15:57:33 +0900

    Adds documentation to all UITextField methods.

--- Succinct/UIView/UIView+UITextField.swift
+++ Succinct/UIView/UIView+UITextField.swift
@@ -2,18 +2,50 @@ import UIKit
 
 // MARK: - Has UITextFields
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose placeholder text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(withExactPlaceholderText searchText: String) -> Bool {
         return findTextField(withExactPlaceholderText: searchText).isNotNil()
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose placeholder text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(containingPlaceholderText searchText: String) -> Bool {
         return findTextField(containingPlaceholderText: searchText).isNotNil()
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(withExactText searchText: String) -> Bool {
         return findTextField(withExactText: searchText).isNotNil()
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(containingText searchText: String) -> Bool {
         return findTextField(containingText: searchText).isNotNil()
     }
@@ -21,24 +53,56 @@ extension UIView {
 
 // MARK: - Finding UITextFields
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose placeholder text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(withExactPlaceholderText searchText: String) -> UITextField? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextField(withExactPlaceholderText: searchText) }
         ) as? UITextField
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose placeholder text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(containingPlaceholderText searchText: String) -> UITextField? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextField(containingPlaceholderText: searchText) }
         ) as? UITextField
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(withExactText searchText: String) -> UITextField? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextField(withExactText: searchText) }
         ) as? UITextField
     }
 
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextField whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(containingText searchText: String) -> UITextField? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextField(containingText: searchText) }
--- Succinct/UIViewController/UIViewController+UITextField.swift
+++ Succinct/UIViewController/UIViewController+UITextField.swift
@@ -2,18 +2,50 @@ import UIKit
 
 // MARK: - Has UITextField
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose placeholder text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(withExactPlaceholderText searchText: String) -> Bool {
         return view.hasTextField(withExactPlaceholderText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose placeholder text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(containingPlaceholderText searchText: String) -> Bool {
         return view.hasTextField(containingPlaceholderText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(withExactText searchText: String) -> Bool {
         return view.hasTextField(withExactText: searchText)
     }
     
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: A boolean value indicating if a UITextField object was found or not.
+    ///
     public func hasTextField(containingText searchText: String) -> Bool {
         return view.hasTextField(containingText: searchText)
     }
@@ -21,18 +53,50 @@ extension UIViewController {
 
 // MARK: - Finding UITextField
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose placeholder text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(withExactPlaceholderText searchText: String) -> UITextField? {
         return view.findTextField(withExactPlaceholderText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose placeholder text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the placeholder text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(containingPlaceholderText searchText: String) -> UITextField? {
         return view.findTextField(containingPlaceholderText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(withExactText searchText: String) -> UITextField? {
         return view.findTextField(withExactText: searchText)
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextField whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextField objects that are found.
+    /// - Returns: An optional UITextField, if one is found.
+    ///
     public func findTextField(containingText searchText: String) -> UITextField? {
         return view.findTextField(containingText: searchText)
     }
