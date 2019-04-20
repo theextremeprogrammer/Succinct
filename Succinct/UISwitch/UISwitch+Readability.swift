abc9990ef7eeb972befce188f2208a8a3eb5d2bf
Derek Lee <dereklee@downtothis.com>
2019-04-20 18:01:37 +0900

    Adds documentation to UISwitch convenience property
    Closes #37

--- Succinct/UISwitch/UISwitch+Readability.swift
+++ Succinct/UISwitch/UISwitch+Readability.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 extension UISwitch {
+    ///
+    /// Convenience method which indicates if the UISwitch is off. This can be helpful when writing assertions as opposed to testing the isOn property.
+    ///
     public var isOff: Bool {
         get {
             return !isOn
