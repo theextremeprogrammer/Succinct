70ed7b0ec76338f6d64b4133b1cb50a323ace7c6
Derek Lee <dereklee@downtothis.com>
2019-04-20 15:49:22 +0900

    Adds documentation to Optional convenience methods.

--- Succinct/Optional/Optional+NilCheck.swift
+++ Succinct/Optional/Optional+NilCheck.swift
@@ -1,8 +1,18 @@
 extension Optional {
+    ///
+    /// Convenience method to check if the current optional value is nil.
+    ///
+    /// - Returns: True if the optional is nil, false otherwise.
+    ///
     public func isNil() -> Bool {
         return self == nil
     }
 
+    ///
+    /// Convenience method to check if the current optional value is **not** nil.
+    ///
+    /// - Returns: True if the optional is **not** nil, false otherwise.
+    ///
     public func isNotNil() -> Bool {
         return self != nil
     }
