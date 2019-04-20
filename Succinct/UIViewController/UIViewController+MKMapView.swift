921e8d2cc68100b952cb14aef94d61d944d6e222
Derek Lee <dereklee@downtothis.com>
2019-04-20 15:23:09 +0900

    Adds documentation for MKMapView methods

--- Succinct/UIViewController/UIViewController+MKMapView.swift
+++ Succinct/UIViewController/UIViewController+MKMapView.swift
@@ -1,10 +1,24 @@
 import MapKit
 
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for an MKMapView object.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Returns: A boolean value indicating if an MKMapView was found or not.
+    ///
     public func hasMapView() -> Bool {
         return view.findMapView().isNotNil()
     }
 
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for an MKMapView object.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Returns: An optional MKMapView, if one is found.
+    ///
     public func findMapView() -> MKMapView? {
         return view.findMapView()
     }
