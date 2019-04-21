fd0ceac8b82341c1e9f704dfd1e1b557268ddbcd
Derek Lee <dereklee@downtothis.com>
2019-04-21 21:40:12 +0900

    Adds documentation for all UITextView related methods

--- Succinct/UIView/UIView+UITextView.swift
+++ Succinct/UIView/UIView+UITextView.swift
@@ -3,14 +3,27 @@ import UIKit
 
 // MARK: - Has UITextView
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
     public func hasTextView(withExactText searchText: String) -> Bool {
         return findTextView(withExactText: searchText).isNotNil()
     }
 
-    public func hasTextView(containingText searchText: String) -> Bool {
-        return findTextView(containingText: searchText).isNotNil()
-    }
-
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text matches the searchText exactly and the provided searchAttributes.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Parameter searchAttributes: Attributes to compare to the attributes of the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
     public func hasTextView(
         withExactText searchText: String,
         matchingAttributes searchAttributes: [NSAttributedString.Key : Any]
@@ -19,28 +32,65 @@ extension UIView {
             satisfyingCondition: { $0.isTextView(withExactText: searchText, matchingAttributes: searchAttributes) }
             ).isNotNil()
     }
+
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
+    public func hasTextView(containingText searchText: String) -> Bool {
+        return findTextView(containingText: searchText).isNotNil()
+    }
 }
 
 // MARK: - Finding UITextView
 extension UIView {
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
     public func findTextView(withExactText searchText: String) -> UITextView? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextView(withExactText: searchText) }
         ) as? UITextView
     }
 
-    public func findTextView(containingText searchText: String) -> UITextView? {
-        return findInSubviews(
-            satisfyingCondition: { $0.isTextView(containingText: searchText) }
-        ) as? UITextView
-    }
-
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text matches the searchText exactly and the provided searchAttributes.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Parameter searchAttributes: Attributes to compare to the attributes of the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
     public func findTextView(
         withExactText searchText: String,
         matchingAttributes searchAttributes: [NSAttributedString.Key : Any]
     ) -> UITextView? {
         return findInSubviews(
             satisfyingCondition: { $0.isTextView(withExactText: searchText, matchingAttributes: searchAttributes) }
+        ) as? UITextView
+    }
+
+    ///
+    /// Searches the entire view hierarchy of the current view for a UITextView whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
+    public func findTextView(containingText searchText: String) -> UITextView? {
+        return findInSubviews(
+            satisfyingCondition: { $0.isTextView(containingText: searchText) }
         ) as? UITextView
     }
 }
@@ -58,18 +108,6 @@ fileprivate extension UIView {
         return text == searchText
     }
 
-    func isTextView(containingText searchText: String) -> Bool {
-        guard let textfield = self as? UITextView else {
-            return false
-        }
-
-        guard let text = textfield.text else {
-            return false
-        }
-
-        return text.contains(searchText)
-    }
-
     func isTextView(
         withExactText searchText: String,
         matchingAttributes searchAttributes: [NSAttributedString.Key : Any]
@@ -83,5 +121,17 @@ fileprivate extension UIView {
         }
 
         return attributedText.containsExactString(searchText, withAttributes: searchAttributes)
+    }
+
+    func isTextView(containingText searchText: String) -> Bool {
+        guard let textfield = self as? UITextView else {
+            return false
+        }
+
+        guard let text = textfield.text else {
+            return false
+        }
+
+        return text.contains(searchText)
     }
 }
--- Succinct/UIViewController/UIViewController+UITextView.swift
+++ Succinct/UIViewController/UIViewController+UITextView.swift
@@ -2,36 +2,86 @@ import UIKit
 
 // MARK: - Has UITextView
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
     public func hasTextView(withExactText searchText: String) -> Bool {
         return view.hasTextView(withExactText: searchText)
     }
 
-    public func hasTextView(containingText searchText: String) -> Bool {
-        return view.hasTextView(containingText: searchText)
-    }
-
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text matches the searchText exactly and the provided searchAttributes.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Parameter searchAttributes: Attributes to compare to the attributes of the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
     public func hasTextView(
         withExactText searchText: String,
         matchingAttributes searchAttributes: [NSAttributedString.Key : Any]
     ) -> Bool {
         return view.hasTextView(withExactText: searchText, matchingAttributes: searchAttributes)
     }
+
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: A boolean value indicating if a UITextView object was found or not.
+    ///
+    public func hasTextView(containingText searchText: String) -> Bool {
+        return view.hasTextView(containingText: searchText)
+    }
 }
 
 // MARK: - Finding UITextView
 extension UIViewController {
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text matches the searchText exactly.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
     public func findTextView(withExactText searchText: String) -> UITextView? {
         return view.findTextView(withExactText: searchText)
     }
 
-    public func findTextView(containingText searchText: String) -> UITextView? {
-        return view.findTextView(containingText: searchText)
-    }
-
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text matches the searchText exactly and the provided searchAttributes.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Parameter searchAttributes: Attributes to compare to the attributes of the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
     public func findTextView(
         withExactText searchText: String,
         matchingAttributes searchAttributes: [NSAttributedString.Key : Any]
     ) -> UITextView? {
         return view.findTextView(withExactText: searchText, matchingAttributes: searchAttributes)
+    }
+
+    ///
+    /// Searches the entire view hierarchy of a view controller's primary view for a UITextView whose text contains the searchText.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchText: Text to compare to the text of any UITextView objects that are found.
+    /// - Returns: An optional UITextView, if one is found.
+    ///
+    public func findTextView(containingText searchText: String) -> UITextView? {
+        return view.findTextView(containingText: searchText)
     }
 }
