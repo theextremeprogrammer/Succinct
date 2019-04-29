6ef2de4a1d5e6b9e5feb976478ce75cccb5ad8ef
Derek Lee <dereklee@downtothis.com>
2019-04-29 20:42:33 +0900

    Removes 'self.' in UIView+UIPicker implementation by renaming variables

--- Succinct/UIPickerView/UIPickerView+SelectComponent.swift
+++ Succinct/UIPickerView/UIPickerView+SelectComponent.swift
@@ -2,32 +2,32 @@ import UIKit
 
 extension UIPickerView {
     public func selectRow(columnIndex: Int, withExactText searchText: String) {
-        let numberOfRows = self.numberOfRows(inComponent: columnIndex)
+        let numberOfRowsForColumn = numberOfRows(inComponent: columnIndex)
 
-        for rowIndex in 0...numberOfRows - 1 {
-            let rowTitle = self.delegate?.pickerView?(self, titleForRow: rowIndex, forComponent: columnIndex)
+        for rowIndex in 0...numberOfRowsForColumn - 1 {
+            let rowTitle = delegate?.pickerView?(self, titleForRow: rowIndex, forComponent: columnIndex)
 
             if rowTitle == searchText {
-                self.delegate?.pickerView?(self, didSelectRow: rowIndex, inComponent: columnIndex)
+                delegate?.pickerView?(self, didSelectRow: rowIndex, inComponent: columnIndex)
                 return
             }
         }
     }
 
     public func selectRow(withExactText searchText: String) {
-        for componentIndex in 0...self.numberOfComponents - 1 {
+        for componentIndex in 0...numberOfComponents - 1 {
             selectRow(columnIndex: componentIndex, withExactText: searchText)
         }
     }
 
     public func hasSelectedRow(withExactText searchText: String) -> Bool {
-        for componentIndex in 0...self.numberOfComponents - 1 {
-            let selectedRow = self.selectedRow(inComponent: componentIndex)
+        for columnIndex in 0...numberOfComponents - 1 {
+            let selectedRowForColumn = selectedRow(inComponent: columnIndex)
 
-            let selectedRowTitle = self.delegate?.pickerView?(
+            let selectedRowTitle = delegate?.pickerView?(
                 self,
-                titleForRow: selectedRow,
-                forComponent: componentIndex
+                titleForRow: selectedRowForColumn,
+                forComponent: columnIndex
             )
 
             if selectedRowTitle == searchText {
@@ -39,11 +39,11 @@ extension UIPickerView {
     }
 
     public func hasRow(withExactText searchText: String) -> Bool {
-        for componentIndex in 0...self.numberOfComponents - 1 {
-            let numberOfRows = self.numberOfRows(inComponent: componentIndex)
+        for columnIndex in 0...numberOfComponents - 1 {
+            let numberOfRowsForColumn = numberOfRows(inComponent: columnIndex)
 
-            for rowIndex in 0...numberOfRows - 1 {
-                let rowTitle = self.delegate?.pickerView?(self, titleForRow: rowIndex, forComponent: componentIndex)
+            for rowIndex in 0...numberOfRowsForColumn - 1 {
+                let rowTitle = delegate?.pickerView?(self, titleForRow: rowIndex, forComponent: columnIndex)
 
                 if rowTitle == searchText {
                     return true
