dbfad999d531afc4aeafd0fe580d681d8fd3d337
Derek Lee <dereklee@downtothis.com>
2019-04-30 16:27:38 +0900

    Adds documentation for UITabBarController.selectTab()

--- Succinct/UITabBarController/UITabBarController+SelectTab.swift
+++ Succinct/UITabBarController/UITabBarController+SelectTab.swift
@@ -1,7 +1,11 @@
 import UIKit
 
 extension UITabBarController {
-    public func selectTab(withTitle searchTitle: String) {
+    ///
+    /// Attempts to select a tab in a UITabBarController whose UITabBarItem `title` property matches the searchTitle text.
+    ///
+    /// - Parameter searchTitle: Text to compare to the title text of any UITabBarItem objects that are found.
+    ///
     public func selectTab(withExactTitle searchTitle: String) {
         if let tabBarItems = tabBar.items {
             for index in 0..<tabBarItems.count {
