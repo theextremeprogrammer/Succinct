57252637e2d27b21782f6872ec3cdc97a3e9da9a
Derek Lee <dereklee@downtothis.com>
2019-05-01 20:48:23 +0900

    Documents tapping UISwitch objects

--- Succinct/UISwitch/UISwitch+Tap.swift
+++ Succinct/UISwitch/UISwitch+Tap.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 extension UISwitch {
+    ///
+    /// Simulates tapping a UISwitch control by sending the `.touchUpInside` event action. Since simply sending the action does not change the state of the UISwitch, this method also flips the `isOn` property to simulate the effect tapping the switch would actually have.
+    ///
     public func tapAndFireTargetEvent() {
         isOn = !isOn
         sendActions(for: .touchUpInside)
