cb2796822f4df425755614b10291f2a313ef9da2
Derek Lee <dereklee@downtothis.com>
2019-05-01 21:15:21 +0900

    Adds documentation for UICollectionView methods

--- Succinct/UICollectionView/UICollectionView+Evaluations.swift
+++ Succinct/UICollectionView/UICollectionView+Evaluations.swift
@@ -1,4 +1,12 @@
 extension UICollectionView {
+    ///
+    /// Searches through all cells of all sections within a UICollectionView for a cell that satisfies the passed in condition.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter satisfyingCondition: A closure that takes a UICollectionViewCell as an argument and evaluates it against some kind of condition and returns a boolean value if a cell is found that matches the condition.
+    /// - Returns: A boolean value indicating if a UICollectionViewCell was found or not.
+    ///
     func hasCell(satisfyingCondition: (UICollectionViewCell) -> Bool) -> Bool {
         for section in 0..<numberOfSections {
             let numberOfItems = self.numberOfItems(inSection: section)
