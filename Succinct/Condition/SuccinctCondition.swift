636fb21dd5aa08c1434f11be05fb5975fb4590fa
Derek Lee <dereklee@downtothis.com>
2019-07-04 12:33:21 -0600

    Clean up some documentation

--- Succinct/Condition/SuccinctCondition.swift
+++ Succinct/Condition/SuccinctCondition.swift
@@ -1,11 +1,10 @@
 ///
 /// A SuccinctCondition encapsulates the conditional logic that should be applied when evaluating a UIView. The evaluation of this condition returns an EvaluationResult object which can indicate not only if the UIView met the specified condition, but for instances when the condition is not met then additional information can be communicated.
 ///
-/// Additionally, but encapsulating the conditional logic in this object, additional processing can be performed for all conditionos, such as debug logging.
+/// Additionally, by encapsulating the conditional logic in this object, additional processing can be performed for all conditionos, such as debug logging.
 ///
 internal struct SuccinctCondition {
-    /// The condition to evaluate against a UIView which will return an EvaluationResult
-    /// which indicates if the condition was successful or not, and if not, why.
+    /// The condition to evaluate against a UIView which will return an EvaluationResult which indicates if the condition was successful or not, and if not, why.
     private let condition: (UIView) -> EvaluationResult
 
     init(_ condition: @escaping (UIView) -> EvaluationResult) {
