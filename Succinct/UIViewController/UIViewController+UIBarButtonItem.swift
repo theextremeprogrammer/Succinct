583b53453b17b3a830a7eed2f37fef06e9baf3ed
Derek Lee <derek@sunrisingappdev.com>
2022-02-12 14:48:36 +0900

    #80 Completed documentation for new hasUIBarButtonItem methods

--- Succinct/UIViewController/UIViewController+UIBarButtonItem.swift
+++ Succinct/UIViewController/UIViewController+UIBarButtonItem.swift
@@ -2,7 +2,16 @@ import UIKit
 
 // MARK: - UIBarButtonItem
 extension UIViewController {
-    // TODO: Documentation
+    ///
+    /// Searches the view controller's `navigationItem.leftBarButtonItems`, `navigationItem.rightBarButtonItems`, as well as the UIView hierarchy for a UIToolbar which may contain a UIBarButtonItem whose `SystemItem` matches the searchSystemItem.
+    ///
+    /// This method is convenient for de-coupling from the details of where a UIBarButtonItem object may be located.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchSystemItem: `UIBarButtonItem.SystemItem` to compare to the UIBarButtonItem objects that are found.
+    /// - Returns: A boolean value indicating if a UIBarButtonItem object was found or not.
+    ///
     @objc public func hasBarButtonItem(withSystemItem searchSystemItem: UIBarButtonItem.SystemItem) -> Bool {
         if let _ = navigationItem.findBarButtonItem(matchingCondition: { $0.systemItem == searchSystemItem }) {
             return true
@@ -15,7 +24,16 @@ extension UIViewController {
         return false
     }
 
-    // TODO: Documentation
+    ///
+    /// Searches the view controller's `navigationItem.leftBarButtonItems`, `navigationItem.rightBarButtonItems` which may contain a UIBarButtonItem whose `Title` matches the searchTitle.
+    ///
+    /// This method is convenient for de-coupling from the details of where a UIBarButtonItem object may be located.
+    ///
+    /// - Note: Once an object is found matching the provided criteria the remainder of the view hierarchy is **not** searched.
+    ///
+    /// - Parameter searchTitle: `String` to compare to the Title strings that are found.
+    /// - Returns: A boolean value indicating if a UIBarButtonItem object was found or not.
+    ///
     @objc public func hasBarButtonItem(withTitle searchTitle: String) -> Bool {
         if let _ = navigationItem.findBarButtonItem(matchingCondition: { $0.title == searchTitle }) {
             return true
